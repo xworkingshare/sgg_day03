3c25dd3fbd6ac8fb42c6d6ffae682d955e9bade5
xworking <643269212@qq.com>
2018-09-22 20:45:27 +0800

    初始化

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall0416/manage/service/impl/ManageServiceImpl.java
@@ -0,0 +1,103 @@
+package com.atguigu.gmall0416.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall0416.bean.*;
+import com.atguigu.gmall0416.manage.mapper.*;
+import com.atguigu.gmall0416.service.ManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class ManageServiceImpl implements ManageService {
+
+    @Autowired
+    private BaseCatalog1Mapper baseCatalog1Mapper;
+
+    @Autowired
+    private BaseCatalog2Mapper baseCatalog2Mapper;
+
+    @Autowired
+    private BaseCatalog3Mapper baseCatalog3Mapper;
+
+    @Autowired
+    private BaseAttrInfoMapper baseAttrInfoMapper;
+
+    @Autowired
+    private BaseAttrValueMapper baseAttrValueMapper;
+
+    @Autowired
+    private SpuInfoMapper spuInfoMapper;
+
+    @Override
+    public List<BaseCatalog1> getCatalog1() {
+        return baseCatalog1Mapper.selectAll();
+    }
+
+    @Override
+    public List<BaseCatalog2> getCatalog2(String catalog1Id) {
+        BaseCatalog2 baseCatalog2 = new BaseCatalog2();
+        baseCatalog2.setCatalog1Id(catalog1Id);
+        return baseCatalog2Mapper.select(baseCatalog2);
+    }
+
+    @Override
+    public List<BaseCatalog3> getCatalog3(String catalog2Id) {
+        BaseCatalog3 baseCatalog3 = new BaseCatalog3();
+        baseCatalog3.setCatalog2Id(catalog2Id);
+        return  baseCatalog3Mapper.select(baseCatalog3);
+    }
+
+    @Override
+    public List<BaseAttrInfo> getAttrList(String catalog3Id) {
+        BaseAttrInfo baseAttrInfo = new BaseAttrInfo();
+        baseAttrInfo.setCatalog3Id(catalog3Id);
+        return baseAttrInfoMapper.select(baseAttrInfo);
+    }
+
+    @Override
+    public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
+        //  判断当前id 是否存在
+        if (baseAttrInfo.getId()!=null && baseAttrInfo.getId().length()>0){
+            baseAttrInfoMapper.updateByPrimaryKey(baseAttrInfo);
+        }else {
+            // 防止baseAttrInfo.getId()="";
+            if (baseAttrInfo.getId().length()==0){
+                baseAttrInfo.setId(null);
+            }
+            baseAttrInfoMapper.insertSelective(baseAttrInfo);
+        }
+        // 插入平台属性值，平台属性值--平台属性的id 有关系，在插入的时候，需要先删除原来id对应的数据，再进行插入。
+        BaseAttrValue baseAttrValue = new BaseAttrValue();
+        // AttrId = baseAttrInfo.Id
+        baseAttrValue.setAttrId(baseAttrInfo.getId());
+        baseAttrValueMapper.delete(baseAttrValue);
+
+        List<BaseAttrValue> attrValueList = baseAttrInfo.getAttrValueList();
+        for (BaseAttrValue attrValue : attrValueList) {
+                if (attrValue.getId().length()==0){
+                    attrValue.setId(null);
+                }
+                // 要取得BaseAttrInfo的id 实际上是取得数据库中自动增长的Id
+                attrValue.setAttrId(baseAttrInfo.getId());
+                baseAttrValueMapper.insertSelective(attrValue);
+            }
+    }
+
+    @Override
+    public BaseAttrInfo getAttrInfo(String attrId) {
+        // 查询到BaseAttrInfo 对象
+        BaseAttrInfo baseAttrInfo = baseAttrInfoMapper.selectByPrimaryKey(attrId);
+        BaseAttrValue baseAttrValue = new BaseAttrValue();
+        baseAttrValue.setAttrId(baseAttrInfo.getId());
+        // 查询baseAttrValue
+        List<BaseAttrValue> attrValueList = baseAttrValueMapper.select(baseAttrValue);
+        baseAttrInfo.setAttrValueList(attrValueList);
+        return baseAttrInfo;
+    }
+
+    @Override
+    public List<SpuInfo> getSpuInfoList(SpuInfo spuInfo) {
+       return spuInfoMapper.select(spuInfo);
+    }
+}
